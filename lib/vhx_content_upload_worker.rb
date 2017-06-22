052551c6998d36993225e4f3828ff95a7cf3d44c
France <hipsterelitist@users.noreply.github.com>
2017-06-22 18:02:17 -0400

    squash those arrays and toss those nils

--- lib/vhx_content_upload_worker.rb
+++ lib/vhx_content_upload_worker.rb
@@ -71,8 +71,9 @@ class VhxContentUploadWorker
   def source_location(vimeo_video)
     return @source_location if @source_location 
     source_files = []
-    source_files += vimeo_video['files']&.select { |link| link['quality'] == 'source' || link['width'] == 1920 }
-    source_files += vimeo_video['download']&.select { |link| link['quality'] == 'source' || link['width'] == 1920 }
+    source_files += Array(vimeo_video['files']&.select { |link| link['quality'] == 'source' || link['height'] == 1920 })
+    source_files += Array(vimeo_video['download']&.select { |link| link['quality'] == 'source' || link['height'] == 1920 })
+    source_files = source_files.flatten.compact
     raise 'No quality files' if source_files.empty?
     source_file = source_files.sort_by { |link| link['quality'] }.last
     interim_location = source_file['link_secure'] 
